23d7a2b8fa6973c2a61601a3a4bbfeca556c9358
Leonid Slavutin <slavutin.lv@gmail.com>
2022-07-22 14:06:47 +0200

    Autotest: push 2

---
+++ cypress/Project 1/cypress/e2e/3-test/test_progress_bar.cy.js
@@ -0,0 +1,15 @@
+describe('test click start', () => {
+  it('passes', () => {
+    cy.visit('http://uitestingplayground.com/progressbar')
+    cy.get('#startButton').contains('Start').click()
+    cy.get('#progressBar').contains('75%').then(($div) => {
+      const percentage_result = $div.text()
+      expect(percentage_result).to.eq('75%')
+      cy.get('#stopButton').contains('Stop').click()
+      cy.get('#content').then(($p) => {
+        const delta_time_result = parseFloat($p.text()[8])
+        expect(delta_time_result).to.eq(0)
+      })
+    })
+  })
+})
